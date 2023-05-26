81eb7044e327e0381bd39ef4a42916cfadb50072
Adri12ana <adriana.filipiak@gmail.com>
2023-05-26 17:31:27 +0200

    Dummy var

--- test/testpet.ts
+++ test/testpet.ts
@@ -2,11 +2,13 @@
 
 const request = require('supertest');
 const expect = require('chai').expect;
-describe('POST /pet/{petId}/uploadImage', function() {
-  it('test',async () => {
-   const response = await request('https://petstore.swagger.io/v2')
-    .get('/pet/1');
-console.log(response.statusCode)
+describe('POST /pet/{petId}/uploadImage', function () {
+  it('test', async () => {
+    const response = await request('https://petstore.swagger.io/v2')
+      .get('/pet/1');
+    console.log(response.statusCode)
     expect(response.statusCode).to.be.equal(200)
   })
 });
+
+const tests = "";
